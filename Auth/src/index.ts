fb0322f6e0a205f51570356f70816a75302d82ee
Ashish <sharma.ashish78@gmail.com>
2025-06-17 13:53:16 -0700

    Routing

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,12 +1,17 @@
 import  express  from "express";
 import { json} from 'body-parser';
 
+import { currentUserRouter } from './Routes/current-user';
+import { signinRouter } from './Routes/signin';
+import { signupRouter } from './Routes/signup'; 
+import { singoutRouter } from './Routes/signout';
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi There !!');
-})
-  app.listen(3000, () => {
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);  
+app.use(singoutRouter);
+app.listen(3000, () => {
   console.log("Auth service is running on port 3000!!!");
 });
