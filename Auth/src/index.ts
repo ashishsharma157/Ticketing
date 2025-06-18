4baa029ceb15b4b659d4e7e0bfa83eb7e5e003d8
Ashish <sharma.ashish78@gmail.com>
2025-06-18 13:58:07 -0700

    error handling

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,10 +1,13 @@
 import  express  from "express";
+import 'express-async-errors';
 import { json} from 'body-parser';
 
 import { currentUserRouter } from './Routes/current-user';
 import { signinRouter } from './Routes/signin';
 import { signupRouter } from './Routes/signup'; 
 import { singoutRouter } from './Routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found';
 const app = express();
 app.use(json());
 
@@ -12,6 +15,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);  
 app.use(singoutRouter);
+app.all('*', async (req, res)=>{
+  throw new NotFoundError();
+})
+app.use(errorHandler);
 app.listen(3000, () => {
   console.log("Auth service is running on port 3000!!!");
 });
