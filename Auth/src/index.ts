51d9fd7a7b5a3eb0cc7e766250d4352daa25eb2e
Ashish <sharma.ashish78@gmail.com>
2025-06-20 11:54:13 -0700

    add mongo db

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,6 +1,7 @@
 import  express  from "express";
 import 'express-async-errors';
 import { json} from 'body-parser';
+import mongoose from "mongoose";
 
 import { currentUserRouter } from './Routes/current-user';
 import { signinRouter } from './Routes/signin';
@@ -19,6 +20,17 @@ app.all('*', async (req, res)=>{
   throw new NotFoundError();
 })
 app.use(errorHandler);
+const start = async () => {
+  try{
+  await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  console.log("Connected to MongoDB");
+  }
+  catch (err) {
+    console.error(err);
+  }
+};
 app.listen(3000, () => {
   console.log("Auth service is running on port 3000!!!");
 });
+
+start();
